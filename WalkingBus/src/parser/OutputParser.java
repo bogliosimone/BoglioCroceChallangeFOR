1d40d4d85818aec183918ae0e66b7937d809b9b0
Simone Boglio <bogliosimone@gmail.com>
2017-02-17 03:23:09 +0100

    fix output parse error (+ a little surprise :D)

--- WalkingBus/src/parser/OutputParser.java
+++ WalkingBus/src/parser/OutputParser.java
@@ -1,9 +1,54 @@
 package parser;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 import src.Risultato;
+import src.Strada;
 
 public class OutputParser {
 	public static void OutputFile(String filePath, Risultato risultato){
-		
+		List<Strada> list = risultato.getStrade();
+		String fileString = new String();
+		for(Strada s: list){
+			fileString += s.getArrivo().getId() + " " + s.getPartenza().getId() + "\n";
+		}
+		String fileName =  FilenameUtils.getBaseName(filePath)+".sol";
+		File file = new File(fileName);
+		try {
+			FileUtils.writeStringToFile(file,fileString, "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//:D uoooooooooh faccio anche il disegnino
+		drawGraph(risultato);
 	}
+		
+		public static void drawGraph(Risultato ris) {
+			Graph graph = new SingleGraph("Tutorial 1");
+			List<Strada> list = ris.getStrade();
+			for(Strada s: list){
+				try{
+				graph.addNode(Integer.toString(s.getArrivo().getId()))
+				.setAttribute("xyz",s.getArrivo().getCoord().getX(),
+						s.getArrivo().getCoord().getY(),0);
+				}catch(Exception e){}
+				try{
+				graph.addNode(Integer.toString(s.getPartenza().getId()))
+				.setAttribute("xyz",s.getPartenza().getCoord().getX(),
+						s.getPartenza().getCoord().getY(),0);
+				}catch(Exception e){}
+				try{
+				graph.addEdge(Integer.toString(s.getArrivo().getId())+Integer.toString(s.getPartenza().getId()), Integer.toString(s.getArrivo().getId()), Integer.toString(s.getPartenza().getId()));
+				}catch(Exception e){}
+			}
+			graph.getNode(0).setAttribute("ui.style", "fill-color: red;");
+			graph.display(false);
+		}
 }
